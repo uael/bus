dbfc52d8097b33619c56b194ff1313c64f39a843
scherrer <scherrer.bastien@gmail.com>
2016-10-30 22:02:33 +0100

    add diagramme sequence UML

---
+++ src/test/java/unice/s3a/bus/command/monScenario.java
@@ -0,0 +1,12 @@
+package unice.s3a.bus.command;
+
+/**
+ * Created by root on 10/30/16.
+ */
+public class monSenario {
+
+    public static void main(String[] args)throws InterruptedException{
+
+        
+    }
+}
